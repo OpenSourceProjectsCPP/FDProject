ad2a47a03965b3b1cb2dd79bc3008b0c362cb0fc
Anuran Datta <anuran.datta@hotmail.com>
2026-01-05 03:51:45 +0530

    card id varied

--- src/core/pipeline.cpp
+++ src/core/pipeline.cpp
@@ -1,32 +1,26 @@
 #include "core/pipeline.h"
 
-
 void Pipeline::start() {
-	running_ = true;
-	worker_ = std::thread(&Pipeline::run, this);
+  running_ = true;
+  worker_ = std::thread(&Pipeline::run, this);
 }
-
 
 void Pipeline::stop() {
-	running_ = false;
-	if (worker_.joinable()) worker_.join();
+  running_ = false;
+  if (worker_.joinable()) worker_.join();
 }
 
-
 void Pipeline::run() {
-	while (running_) {
-		auto opt = queue_.pop();
-		if (!opt) continue;
+  while (running_) {
+    auto opt = queue_.pop();
+    if (!opt) continue;
 
+    const Transaction& txn = *opt;
+    auto features = feature_engine_.extract(txn);
 
-		const Transaction& txn = *opt;
-		auto features = feature_engine_.extract(txn);
-
-
-		std::cout << "Txn " << txn.txn_id
-			<< " f1=" << features.get("txn_count_1min")
-			<< " f5=" << features.get("txn_count_5min")
-			<< " amt5=" << features.get("amount_sum_5min")
-			<< '\n';
-	}
+    std::cout << "Txn " << txn.txn_id << " Card=" << txn.card_id
+              << " f1=" << features.get("txn_count_1min")
+              << " f5=" << features.get("txn_count_5min")
+              << " amt5=" << features.get("amount_sum_5min") << '\n';
+  }
 }
